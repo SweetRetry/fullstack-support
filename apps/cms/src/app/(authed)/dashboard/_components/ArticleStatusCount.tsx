7410b544628add2c48be3e34a7d37c80ba724d07
SweetRetry <zimin.zhang2000@gmail.com>
2024-09-24 16:47:45 +0800

    feat: delete user anlytics

--- apps/cms/src/app/(authed)/dashboard/_components/ArticleStatusCount.tsx
+++ apps/cms/src/app/(authed)/dashboard/_components/ArticleStatusCount.tsx
@@ -1,10 +1,8 @@
-import { ArticleStatus } from "@prisma/client";
 import { getArticleStatusCount } from "@repo/database/services/article";
-import { Book, User } from "lucide-react";
+import { Book } from "lucide-react";
 import React, { useEffect, useState } from "react";
 
 const ArticleStatusCount = () => {
-  const [lodaing, setLodaing] = useState(false);
   const [data, setData] = useState<{
     total: number;
     yesterday: number;
@@ -13,12 +11,10 @@ const ArticleStatusCount = () => {
 
   useEffect(() => {
     async function run() {
-      setLodaing(true);
       const res = await getArticleStatusCount();
       if (res.data) {
         setData(res.data);
       }
-      setLodaing(false);
     }
 
     run();
@@ -35,39 +31,6 @@ const ArticleStatusCount = () => {
           <p className="text-muted-foreground">
             + {data?.increase} from yesterday
           </p>
-        </li>
-      </div>
-
-      <div className="basis-1/4 px-4">
-        <li className="rounded border border-border p-4">
-          <div className="mb-4 flex items-center justify-between">
-            <p>Visits</p>
-            <User />
-          </div>
-          <p className="text-2xl font-bold">1000 visits</p>
-          <p className="text-muted-foreground">+ 88 from yesterday</p>
-        </li>
-      </div>
-
-      <div className="basis-1/4 px-4">
-        <li className="rounded border border-border p-4">
-          <div className="mb-4 flex items-center justify-between">
-            <p>Visits</p>
-            <User />
-          </div>
-          <p className="text-2xl font-bold">1000 visits</p>
-          <p className="text-muted-foreground">+ 88 from yesterday</p>
-        </li>
-      </div>
-
-      <div className="basis-1/4 px-4">
-        <li className="rounded border border-border p-4">
-          <div className="mb-4 flex items-center justify-between">
-            <p>Visits</p>
-            <User />
-          </div>
-          <p className="text-2xl font-bold">1000 visits</p>
-          <p className="text-muted-foreground">+ 88 from yesterday</p>
         </li>
       </div>
     </ul>
